b0b19585a40560347e023bfbdd8d243fd3912c9d
SilvioMachado <silvio.machado131@gmail.com>
2020-05-09 21:10:37 -0300

    making loing

---
+++ src/api/login.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export const makeLogin = (username, password, onSuccess, onError) => {
+  const params = new FormData();
+  params.append('email', username);
+  params.append('password', password);
+  params.append('origin', 'supera-pwa');
+  
+  const request = new Request(`${process.env.REACT_APP_PHP_URL}/v1/login.php`, {
+    method: 'POST',
+    withCredentials: true,
+    credentials: 'include',
+    body: params
+  });
+
+  fetch(request)
+    .then( async r => {
+      const response = await r.json();
+
+      if (response.success) {
+        onSuccess(response.data);
+      } else {
+        onError(response.error);
+      }
+    }).catch(e => onError(e));
+};
--- src/login/index.js
+++ src/login/index.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+// Components
+import LoadingDialog from 'components/loading-dialog';
+
+// API
+import { makeLogin } from 'api/login';
+
+// Images
 import superaLogo from 'images/supera_logo.png';
 import ic_email from 'images/ic-email.png';
 import ic_password from 'images/ic_password.png';
 import 'css/login.scss';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const onLoginSuccess = (data) => { 
+    console.log(data)
+    setLoading(false);
+  };
+  const onLoginError = (e) => {
+    console.log(e)
+    setLoading(false);
+  };
+
+  const login = () => {
+    setLoading(true);
+    makeLogin(email, password, onLoginSuccess, onLoginError);
+  };
+
   return (
     <div className='root-div'>
+
+      <LoadingDialog open={loading} customMessage={'Entrando...'}/>
+
        <div className='wrapper'>
       
           <img src={superaLogo} /> 
@@ -18,21 +47,23 @@ const Login = () => {
                 <div className='image-div'>
                   <img className='form-image' src={ic_email}/>
                 </div>
-                <input type='text' />
+                <input type='text' onChange={e => setEmail(e.target.value)}/>
               </div>
 
               {/* Password */}
               <div className='login-form'>
+
                 <div className='image-div'>
                   <img className='form-image' src={ic_password} style={{width: '15px'}}/>
                 </div>
-                <input type='password' />
+
+                <input type='password' onChange={e => setPassword(e.target.value)}/>
               </div>
               
               <div className='login-form'><h6>Esqueci a senha</h6></div>
           </div>
 
-          <button className='white-round-button'> Entrar </button>
+          <button className='white-round-button' onClick={login}> Entrar </button>
           <button className='orange-round-button'> Registrar </button>
         </div> 
       
