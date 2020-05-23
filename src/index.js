27c6e3ce497baeb6a286a534dd5be191f1610704
SilvioMachado <silvio.machado131@gmail.com>
2020-05-23 17:08:46 -0300

    using default service worker for offline use

--- src/index.js
+++ src/index.js
@@ -32,7 +32,12 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// navigator.serviceWorker.getRegistration().then(async reg => {
+//   const x = await reg.pushManager.subscribe({userVisibleOnly: true});
+//   console.log(x);
+// })
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.registerServiceWorker();
+serviceWorker.register();
