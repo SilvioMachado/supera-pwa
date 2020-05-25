ecf8fd67c205ce7d2d4f2efe4b88d6fd4063dc9d
Silvio Machado <silvio.machado131@gmail.com>
2020-05-25 15:32:10 -0300

    made app work offline with default service worker

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -45,7 +45,7 @@ export function register(config) {
     window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
-      if (isLocalhost) {
+      // if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
 
@@ -57,7 +57,7 @@ export function register(config) {
               'worker. To learn more, visit https://bit.ly/CRA-PWA'
           );
         });
-      } 
+      // } 
     });
   }
 }
