bd4705727980126abe629f76da6e6deb3b06fa48
Silvio Machado <silvio.machado131@gmail.com>
2021-08-31 20:42:01 -0300

    storybook and menu item

---
+++ src/components/menu-item.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const buttonStyle = {
+  border: '3px solid #F56C27',
+  borderRadius: '0.5em',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  color: '#F56C27',
+  fontWeight: 'bold',
+  margin: '0 10px 20px 10px'
+};
+
+const mediumButton = {
+  ...buttonStyle,
+  width: '100px',
+  height: '100px'
+}
+
+const MenuItem = ({text, imgSource, ...props}) => {
+  return (
+    <div style={mediumButton}>
+      <img src={imgSource} style={{width: '70px', height: '70px'}} />
+      {text}
+    </div>
+  )
+}
+
+export default MenuItem;
---
+++ src/stories/menu-item.stories.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import MenuItem from '../components/menu-item';
+
+import playIcon from 'images/icon-play.svg';
+
+export default {
+  title: 'Menu Item',
+  component: MenuItem
+}
+
+const Template = (args) => <MenuItem {...args} />
+
+export const Default = Template.bind({});
+Default.args = {
+  imgSource: playIcon,
+  text: 'JOGAR'
+};
